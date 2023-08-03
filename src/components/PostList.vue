6bcf49eba20641e6abb47a04cf1a4c844bc88897
Skave-a <arrowtata@gmail.com>
2023-08-03 15:03:54 +0300

    feat: Add conditional rendering

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-show="posts.length > 0">
     <h3>List of Posts</h3>
     <post-item
       v-for="post in posts"
@@ -8,6 +8,12 @@
       @remove="$emit('remove', post)"
     />
   </div>
+  <h3
+    v-show="posts.length === 0"
+    style="text-align: center; margin-top: 50px; color: red"
+  >
+    There are no posts
+  </h3>
 </template>
 
 <script lang="ts">
