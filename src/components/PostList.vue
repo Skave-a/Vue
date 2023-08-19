8943ed88cf29fb5ffda490ec660d335ab7fa0099
Skave-a <arrowtata@gmail.com>
2023-08-19 20:50:19 +0300

    feat: add animation

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-show="posts.length > 0">
     <h3>List of Posts</h3>
-    <post-item
-      v-for="post in posts"
-      :key="post.id"
-      :post="post"
-      @remove="handleRemove(post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+        v-for="post in posts"
+        :key="post.id"
+        :post="post"
+        @remove="handleRemove(post)"
+      />
+    </transition-group>
   </div>
   <h3
     v-show="posts.length === 0"
@@ -39,3 +41,22 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.4s;
+}
+</style>
